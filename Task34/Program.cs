9bd3348edb85a3d99f648946c94ce7abc678f852
Dmitriy <trayan@yandex.ru>
2023-06-29 21:43:52 +1100

    Task34 added

---
+++ Task34/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] RandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+int EvenNum(int[] array)
+{
+    int num = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) num = num + 1;
+    }
+    return num;
+}
+Console.Clear();
+Console.WriteLine("Введите размер массива");
+int Size = int.Parse(Console.ReadLine());
+int MinValue = 100;
+int MaxValue = 999;
+int[] Array = RandomArray(Size, MinValue, MaxValue);
+for (int i = 0; i < Array.Length; i++)
+{
+    Console.Write($"{Array[i]} ");
+}
+Console.WriteLine();
+int Num = EvenNum(Array);
+Console.WriteLine($"Количество чётных чисел в массиве - {Num}");
